d3e5707ab5b56dcd0bb6ada5b5430ffb77893ddc
Johan Nordstrom <johan@johanlaptop>
2024-09-17 10:25:16 +0200

    layout

---
+++ src/app/about/page.tsx
@@ -0,0 +1,7 @@
+export default function AboutRoute() {
+    return (
+        <main>
+            <h1 className="text 4-xl font-bold">About</h1>
+        </main>
+    )
+}
---
+++ src/app/test-route/page.tsx
@@ -0,0 +1,7 @@
+export default function TestRoute() {
+    return (
+        <main>
+            <h1 className="text 4-xl font-bold">Test Route</h1>
+        </main>
+    )
+}
